93d0e22e1c87b130144d62c0b2c470811f539b5f
Fernando Zavatto <zavatto.fer@gmail.com>
2021-04-19 19:26:17 -0300

    Removed/replaced most of the initial boilerplate code, added components folder and a veeeery early navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+
+function NavBar() {
+    return(
+        <div className='NavBar'>
+            <li><a href='/home'>Home</a></li>
+            <li><a href='/novedades'>Novedades</a></li>
+            <li><a href='/mightremovelater'>Placeholder</a></li>
+            <li><a href='/contacto'>Contacto</a></li>
+        </div>
+    );
+}
+
+export default NavBar;
